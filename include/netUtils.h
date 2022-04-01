b2d6b5fc9570a662dcccb791151e2b48de2380e4
CosminOctavianPetre <100428943@alumnos.uc3m.es>
2022-04-01 19:51:52 +0200

    did some debugging check todo.md

--- include/netUtils.h
+++ include/netUtils.h
@@ -1,17 +1,18 @@
 #ifndef NETUTILS_H
 #define NETUTILS_H
 
-#define MAX_CONN_BACKLOG 1          /* max number of open client connections */
+#define MAX_CONN_BACKLOG 5          /* max number of open client connections */
 
 /* sending functions */
 int send_common_header(int socket, header_t *header);
-int send_reply_header(int client_socket, reply_t *reply);
-int send_num_items(int client_socket, reply_t *reply);
+int send_reply_header(int socket, reply_t *reply);
+int send_num_items(int socket, reply_t *reply);
 int send_keys(int socket, item_t *item);
 int send_item(int socket, item_t *item);
 
 /* receiving functions */
 int recv_common_header(int client_socket, header_t *header);
+int recv_reply_header(int socket, reply_t *reply);
 int recv_key(int socket, item_t *item);
 int recv_item(int socket, item_t *item);
 
